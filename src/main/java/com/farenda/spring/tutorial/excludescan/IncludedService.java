439d7d9c7351c924496a993ba5b555dcb29f2959
jaruk <jarukopensource@gmail.com>
2017-02-19 11:14:01 +0100

    examples

---
+++ src/main/java/com/farenda/spring/tutorial/excludescan/IncludedService.java
@@ -0,0 +1,7 @@
+package com.farenda.spring.tutorial.excludescan;
+
+/**
+ * Created by jarek on 2017-02-19.
+ */
+public class IncludedService {
+}
---
+++ src/main/java/com/farenda/spring/tutorial/sprintpropertiesfile/SpringLoadPropertiesFile.java
@@ -0,0 +1,7 @@
+package com.farenda.spring.tutorial.sprintpropertiesfile;
+
+/**
+ * Created by jarek on 2017-02-19.
+ */
+public class SpringLoadPropertiesFile {
+}
